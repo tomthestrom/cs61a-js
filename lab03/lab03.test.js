27a699516a878dd5c5fa3bf2e251b8374fd236ea
tomthestrom <thruska29@gmail.com>
2023-03-24 16:49:58 +0100

    lab03/hailstone test

--- lab03/lab03.test.js
+++ lab03/lab03.test.js
@@ -1,7 +1,14 @@
 const lab03 = require("./lab03.js");
 const skipAdd = lab03.skipAdd;
+const hailstone = lab03.hailstone;
 
 test("Skip Add - Takes a number x and returns x + x-2 + x-4 + x-6 + ... + 0", () => {
 	expect(skipAdd(5)).toBe(9);
 	expect(skipAdd(10)).toBe(30);
+})
+
+
+test("Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.", () => {
+	expect(hailstone(10)).toBe(7);
 })
