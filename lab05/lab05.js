2552ed0cb666c6245c02d2c037af81e289958456
tomthestrom <thruska29@gmail.com>
2023-03-24 21:50:32 +0100

    lab05, addChars implemented

--- lab05/lab05.js
+++ lab05/lab05.js
@@ -12,6 +12,22 @@ const merge = function(lst1, lst2) {
 	return [lst2[0]].concat(merge(lst1, lst2.slice(1)))
 }
 
+const addChars = function(w1, w2) {
+	//base cases
+	if (w1 == w2) {
+		return ''
+	} else if (w1.length == 0) {
+		return w2
+	}
+
+	if (w1[0] == w2[0]) {
+		return addChars(w1.slice(1), w2.slice(1));
+	} else {
+		return w2[0] + addChars(w1, w2.slice(1));
+	}
+}
+
 module.exports = {
-	merge
+	merge,
+	addChars
 }
