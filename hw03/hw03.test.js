b27d34c58ee3d0da09a12f642b1b519d402bb24a
tomthestrom <thruska29@gmail.com>
2023-03-27 17:32:13 +0200

    hw03 - functional_tree - tree ADT DS

--- hw03/hw03.test.js
+++ hw03/hw03.test.js
@@ -1,6 +1,11 @@
 const hw03 = require("./hw03.js");
+const treeADT = require("./functional_tree.js");
+
 const flatten = hw03.flatten;
 const flattenReduce = hw03.flattenReduce;
+
+const tree = treeADT.tree;
+const replaceLeaf = hw03.replaceLeaf;
 
 test("Returns a flattened version of lst.", () => {
 	expect(flatten([1, 2, 3])).toEqual([1, 2, 3]);
@@ -16,3 +21,28 @@ test("Returns a flattened version of lst. - Reduce one-liner", () => {
 	expect(flattenReduce([[1, [1, 1]], 1, [1, 1]])).toEqual([1, 1, 1, 1, 1, 1]);
 })
 
+const yggdrasil = tree('odin',
+    [tree('balder',
+    [tree('thor'),
+    tree('freya')]),
+    tree('frigg',
+    [tree('thor')]),
+    tree('thor',
+    [tree('sif'),
+    tree('thor')]),
+    tree('thor')]);
+
+const expected = tree('odin',
+    [tree('balder',
+    [tree('freya'),
+    tree('freya')]),
+    tree('frigg',
+    [tree('freya')]),
+    tree('thor',
+    [tree('sif'),
+    tree('freya')]),
+    tree('freya')]);
+
+test("", () => {
+	expect(replaceLeaf(yggdrasil, 'thor', 'freya')).toEqual(expected);
+})
